154649bc9a86c5ddf47dc01ac91cb82804bcfaaf
Pavel Karateev <lancelote.du.lac@gmail.com>
2025-06-30 18:51:56 +0200

    implement `ParseProgram`

--- parser/parser.go
+++ parser/parser.go
@@ -28,5 +28,16 @@ func (p *Parser) nextToken() {
 }
 
 func (p *Parser) ParseProgram() *ast.Program {
-	return nil
+	program := &ast.Profram{}
+	program.Statements = []ast.Statements{}
+
+	for p.curToken.Type != token.EOF {
+		stmt := p.parseStatement()
+		if stmt != nil {
+			program.Statements = append(program.Statements, stmt)
+		}
+		p.nextToken()
+	}
+
+	return program
 }

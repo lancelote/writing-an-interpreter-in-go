6c5406b02cb509369b86645b83db21c77e75dcfe
Pavel Karateev <lancelote.du.lac@gmail.com>
2025-08-12 23:02:40 +0200

    add basic evaluator

---
+++ evaluator/evaluator_test.go
@@ -0,0 +1,45 @@
+package evaluator
+
+import (
+	"github.com/lancelote/writing-an-interpreter-in-go/lexer"
+	"github.com/lancelote/writing-an-interpreter-in-go/object"
+	"github.com/lancelote/writing-an-interpreter-in-go/parser"
+	"testing"
+)
+
+func TestEvalIntegerExpression(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected int64
+	}{
+		{"5", 5},
+		{"10", 10},
+	}
+
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		testIntegerObject(t, evaluated, tt.expected)
+	}
+}
+
+func testEval(input string) object.Object {
+	l := lexer.New(input)
+	p := parser.New(l)
+	program := p.ParseProgram()
+	return Eval(program)
+}
+
+func testIntegerObject(t *testing.T, obj object.Object, expected int64) bool {
+	result, ok := obj.(*object.Integer)
+	if !ok {
+		t.Errorf("want integer, got %T", obj)
+		return false
+	}
+
+	if result.Value != expected {
+		t.Errorf("want %d, got %d", expected, result.Value)
+		return false
+	}
+
+	return true
+}

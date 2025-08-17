65d6c89af91b5879c09235426e1382538104fe9b
Pavel Karateev <lancelote.du.lac@gmail.com>
2025-08-17 14:22:07 +0200

    add error to `evalIntegerInfixExpression()`

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -127,7 +127,7 @@ func evalIntegerInfixExpression(operator string, left, right object.Object) obje
 	case "!=":
 		return nativeBoolToBooleanObject(leftVal != rightVal)
 	default:
-		return NULL
+		return newError("unknown operator: %s %s %s", left.Type(), operator, right.Type())
 	}
 }
 

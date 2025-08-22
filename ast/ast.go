41059c16a4031e7335c8df750a2b86f45a528f3b
Pavel Karateev <lancelote.du.lac@gmail.com>
2025-08-22 21:06:23 +0200

    add `ArrayLiteral` ast node

--- ast/ast.go
+++ ast/ast.go
@@ -331,3 +331,25 @@ func (sl *StringLiteral) TokenLiteral() string {
 func (sl *StringLiteral) String() string {
 	return sl.Token.Literal
 }
+
+type ArrayLiteral struct {
+	Token    token.Token
+	Elements []Expression
+}
+
+func (al *ArrayLiteral) expressionNode() {}
+
+func (al *ArrayLiteral) TokenLiteral() string {
+	var out bytes.Buffer
+
+	elements := []string{}
+	for _, e := range al.Elements {
+		elements = append(elements, e.String())
+	}
+
+	out.WriteString("[")
+	out.WriteString(strings.Join(elements, ", "))
+	out.WriteString("]")
+
+	return out.String()
+}

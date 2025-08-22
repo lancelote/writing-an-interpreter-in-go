bf9a29483eb229154bf156d9c6d717afd5f1bbc0
Pavel Karateev <lancelote.du.lac@gmail.com>
2025-08-22 15:50:47 +0200

    add test for string literal parsing

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -541,6 +541,29 @@ func TestCallExpressionArgumentParsing(t *testing.T) {
 	}
 }
 
+func TestStringLiteralExpression(t *testing.T) {
+	input := `"hello world";`
+
+	l := lexer.New(input)
+	p := New(l)
+	program := p.ParseProgram()
+
+	checkParseErrors(t, p)
+
+	assertStatementCount(t, program.Statements, 1)
+
+	stmt := assertExpressionStatement(t, program.Statements[0])
+
+	literal, ok := stmt.Expression.(*ast.StringLiteral)
+	if !ok {
+		t.Fatalf("expected string literal, got %T", stmt.Expression)
+	}
+
+	if literal.Value != "hello world" {
+		t.Errorf("expected %q string literal, got %q", "hello world", literal.Value)
+	}
+}
+
 func testLiteralExpression(t *testing.T, exp ast.Expression, expected any) bool {
 	switch v := expected.(type) {
 	case int:

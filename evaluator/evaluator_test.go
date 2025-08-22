97cadc87afb551bbb78894842ea001457a7da206
Pavel Karateev <lancelote.du.lac@gmail.com>
2025-08-22 18:44:16 +0200

    add test for builtin function len

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -305,6 +305,37 @@ func TestStringConcatenation(t *testing.T) {
 	}
 }
 
+func TestBuiltinFunctions(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected any
+	}{
+		{`len("")`, 0},
+		{`len("four")`, 4},
+		{`len("hello world")`, 11},
+		{`len(1)`, "argument to `len` not supported, got INTEGER"},
+		{`len("one", "two")`, "wrong number of arguments, want 1, got 2"},
+	}
+
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+
+		switch expected := tt.expected.(type) {
+		case int:
+			testIntegerObject(t, evaluated, int64(expected))
+		case string:
+			errObj, ok := evaluated.(*object.Error)
+			if !ok {
+				t.Errorf("want error, got %T", evaluated)
+				continue
+			}
+			if errObj.Message != expected {
+				t.Errorf("want error message %q, got %q", expected, errObj.Message)
+			}
+		}
+	}
+}
+
 func testEval(input string) object.Object {
 	l := lexer.New(input)
 	p := parser.New(l)

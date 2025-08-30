b51a88e95c9f855aa747cbf207d37de069aef1c5
Pavel Karateev <lancelote.du.lac@gmail.com>
2025-08-30 18:38:36 +0200

    add `Hashable` interface & implement `Inspect()` for `Hash`

--- object/object.go
+++ object/object.go
@@ -28,6 +28,10 @@ type Object interface {
 	Inspect() string
 }
 
+type Hashable interface {
+	HashKey() HashKey
+}
+
 type HashKey struct {
 	Type  ObjectType
 	Value uint64
@@ -202,4 +206,19 @@ type Hash struct {
 
 func (h *Hash) Type() ObjectType {
 	return HASH_OBJ
+}
+
+func (h *Hash) Inspect() string {
+	var out bytes.Buffer
+
+	pairs := []string{}
+	for _, pair := range h.Pairs {
+		pairs = append(pairs, fmt.Sprintf("%s: %s", pair.Key.Inspect(), pair.Value.Inspect()))
+	}
+
+	out.WriteString("{")
+	out.WriteString(strings.Join(pairs, ", "))
+	out.WriteString("}")
+
+	return out.String()
 }

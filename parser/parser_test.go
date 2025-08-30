06b537ce9af7427da3e1a5f0f95fe66e79ea54ef
Pavel Karateev <lancelote.du.lac@gmail.com>
2025-08-30 16:36:49 +0200

    add test for parsing hash literals with complex expressions as values

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -665,7 +665,7 @@ func TestParsingHashLiteralsStringKeys(t *testing.T) {
 
 	hash, ok := stmt.Expression.(*ast.HashLiteral)
 	if !ok {
-		t.Errorf("want hash literal, got %T", stmt.Expression)
+		t.Fatalf("want hash literal, got %T", stmt.Expression)
 	}
 
 	if len(hash.Pairs) != 3 {
@@ -705,11 +705,61 @@ func TestParsingEmptyHashLiteral(t *testing.T) {
 
 	hash, ok := stmt.Expression.(*ast.HashLiteral)
 	if !ok {
-		t.Errorf("want hash literal, got %T", stmt.Expression)
+		t.Fatalf("want hash literal, got %T", stmt.Expression)
 	}
 
 	if len(hash.Pairs) != 0 {
 		t.Errorf("want 0 pairs, got %d", len(hash.Pairs))
+	}
+}
+
+func TestParsingHashLiteralsWitExpressions(t *testing.T) {
+	input := `{"one": 0 + 1, "two": 10 - 8, "three": 15 / 5}`
+
+	l := lexer.New(input)
+	p := New(l)
+	program := p.ParseProgram()
+
+	checkParseErrors(t, p)
+
+	assertStatementCount(t, program.Statements, 1)
+
+	stmt := assertExpressionStatement(t, program.Statements[0])
+
+	hash, ok := stmt.Expression.(*ast.HashLiteral)
+	if !ok {
+		t.Fatalf("want hash literal, got %T", stmt.Expression)
+	}
+
+	if len(hash.Pairs) != 3 {
+		t.Errorf("want 3 pairs, got %d", len(hash.Pairs))
+	}
+
+	tests := map[string]func(ast.Expression){
+		"one": func(e ast.Expression) {
+			testInfixExpression(t, e, 0, "+", 1)
+		},
+		"two": func(e ast.Expression) {
+			testInfixExpression(t, e, 10, "-", 8)
+		},
+		"three": func(e ast.Expression) {
+			testInfixExpression(t, e, 15, "/", 5)
+		},
+	}
+
+	for key, value := range hash.Pairs {
+		literal, ok := key.(*ast.StringLiteral)
+		if !ok {
+			t.Errorf("want string key, got %T", key)
+			continue
+		}
+
+		testFunc, ok := tests[literal.String()]
+		if !ok {
+			t.Errorf("no test function for key %q found", literal.String())
+		}
+
+		testFunc(value)
 	}
 }
 

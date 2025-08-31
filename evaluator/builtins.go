4ca095ac6941f473a7b11198213f4001403e6bf8
Pavel Karateev <lancelote.du.lac@gmail.com>
2025-08-31 20:42:36 +0200

    add `puts` builtin

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -1,6 +1,7 @@
 package evaluator
 
 import (
+	"fmt"
 	"github.com/lancelote/writing-an-interpreter-in-go/object"
 	"os"
 )
@@ -89,6 +90,15 @@ var builtins = map[string]*object.Builtin{
 			newElements[length] = args[1]
 
 			return &object.Array{Elements: newElements}
+		},
+	},
+	"puts": {
+		Fn: func(args ...object.Object) object.Object {
+			for _, arg := range args {
+				fmt.Println(arg.Inspect())
+			}
+
+			return NULL
 		},
 	},
 	"rest": {

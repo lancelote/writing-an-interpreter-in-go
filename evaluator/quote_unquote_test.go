306e24f054c043b71a3724d2818e24fbb2064248
Pavel Karateev <lancelote.du.lac@gmail.com>
2025-09-04 21:02:43 +0200

    add tests for `unquote`

--- evaluator/quote_unquote_test.go
+++ evaluator/quote_unquote_test.go
@@ -40,3 +40,43 @@ func TestQuote(t *testing.T) {
 		}
 	}
 }
+
+func TestQuoteUnquote(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected string
+	}{
+		{
+			`quote(unquote(4))`,
+			`4`,
+		},
+		{
+			`quote(unquote(4 + 4))`,
+			`8`,
+		},
+		{
+			`quote(8 + unquote(4 + 4))`,
+			`8 + 8`,
+		},
+		{
+			`quote(unquote(4 + 4), 8))`,
+			`8 + 8`,
+		},
+	}
+
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		quote, ok := evaluated.(*object.Quote)
+		if !ok {
+			t.Fatalf("expected quote object, got %T", evaluated)
+		}
+
+		if quote.Node == nil {
+			t.Fatalf("quote object is nil")
+		}
+
+		if quote.Node.String() != tt.expected {
+			t.Errorf("want %q, got %q", tt.expected, quote.Node.String())
+		}
+	}
+}

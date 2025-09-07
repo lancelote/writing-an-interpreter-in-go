2a27a78f034a8aac930eef5e68a5df8e2b8fefd4
Pavel Karateev <lancelote.du.lac@gmail.com>
2025-09-07 16:33:57 +0200

    add `if` expression support to `Modify()`

--- ast/modify.go
+++ ast/modify.go
@@ -24,6 +24,18 @@ func Modify(node Node, modifier ModifierFunc) Node {
 		node.Left, _ = Modify(node.Left, modifier).(Expression)
 		node.Index, _ = Modify(node.Index, modifier).(Expression)
 
+	case *IfExpression:
+		node.Condition, _ = Modify(node.Condition, modifier).(Expression)
+		node.Consequence, _ = Modify(node.Consequence, modifier).(*BlockStatement)
+		if node.Alternative != nil {
+			node.Alternative, _ = Modify(node.Alternative, modifier).(*BlockStatement)
+		}
+
+	case *BlockStatement:
+		for i, stmt := range node.Statements {
+			node.Statements[i], _ = Modify(stmt, modifier).(Statement)
+		}
+
 	}
 
 	return modifier(node)
